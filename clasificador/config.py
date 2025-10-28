f10defe21bffa7893c2911a9a03329ea359b5da8
Crismary Castellanos <crismary04@gmail.com>
2025-10-28 18:59:52 -0300

    fixed config

--- clasificador/config.py
+++ clasificador/config.py
@@ -1,7 +1,7 @@
-INPUT_FILE = "./data/noticias_test.json"
-OUTPUT_FILE = "./data/noticias_etiquetadas_test.json"
+INPUT_FILE = "./data/noticias.json"
+OUTPUT_FILE = "./data/noticias_etiquetadas.json"
 
-LOTE = "20"
-TEMPERATURE = "0.2"
-TIMEOUT = "600"
+LOTE = 20
+TEMPERATURE = 0.2
+TIMEOUT = 600
 TOP_P = 0.9
